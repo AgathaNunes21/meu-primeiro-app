12234f6656e24b71ac81baf6e10c1ff963c5732a
Agatha Nunes da Silva <agathanunes.contato@gmail.com>
2026-08-14 09:20:10 -0300

    Regressão e Reorganização

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -1,62 +1,30 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CurrencyPipe, UpperCasePipe } from '@angular/common';
-import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
-
-
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [CurrencyPipe, UpperCasePipe, PrecoFormatadoPipe],
+  imports: [CurrencyPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {
 
+export class Produto {
   @Input() nome: string = '';
   @Input() preco: number = 0;
   @Output() produtoSelecionado = new EventEmitter<string>();
+  @Output() produtoAdicionado = new EventEmitter<{
+    nome: string;
+    preco: number;
+  }>();
 
   selecionarProduto() {
     this.produtoSelecionado.emit(this.nome);
   }
 
+  adicionarAoCarrinho() {
+    this.produtoAdicionado.emit({
+      nome: this.nome,
+      preco: this.preco,
+    });
+  }
 }
-//=================================================
-//              Aprendi no dia 04.08
-//=================================================
-
-
-//  nome = 'Produto Exemplo';
-//  preco = 149.99;
-
-
-
-//=================================================
-//              Aprendi no dia 04.08
-//=================================================
-
-
-
-//=================================================
-//              Aprendi no dia 05.08
-//=================================================
-//  mostrarPreco = true;
- 
- 
- 
- 
-//  produtos = [
-//   { nome: 'Monitor', preco: 1500 },
-//   { nome: 'Mouse',    preco: 150  },
-//   { nome: 'Teclado',  preco: 250  },
-//   {nome: 'Caixa de Som', preco: 90}
-// ];
-
-
-
-//=================================================
-//              Aprendi no dia 05.08
-//=================================================
-
-
-
