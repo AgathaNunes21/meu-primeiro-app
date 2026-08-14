310486f7ad93eee9f4b0c91a52e30a2ec4abf0af
Agatha Nunes da Silva <agathanunes.contato@gmail.com>
2026-08-14 10:08:18 -0300

    Construção Interface e HeaderMenu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
   import { Component, signal } from '@angular/core';
   import { RouterOutlet, RouterLink } from '@angular/router';
+  import { Header } from './shared/layout/header/header';
   import { usuarioLogado, login, logout } from './core/auth';
  @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink],
+    imports: [RouterOutlet, RouterLink, Header],
     templateUrl: './app.html',
     styleUrl: './app.css',
   })
